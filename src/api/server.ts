f5ea962c1a28c68b30f631d19d7e7ecda64363c5
Ryan <ryamcmorris@gmail.com>
2024-03-18 16:49:18 -0500

    test

--- src/api/server.ts
+++ src/api/server.ts
@@ -2,7 +2,7 @@ const token = "1160de15165b74e03b1e9c13cf2022ae6d645051c0ae9340";
 
 export const server_calls = {
   get: async () => {
-    const response = await fetch(`https://library-api-86an.onrender.com/api/stock`, {
+    const response = await fetch(`http://127.0.0.1:5000/api/stock`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
